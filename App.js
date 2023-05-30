a429bb71db7b222c9969348f5448e051b58e090c
cartolanofacundo <cartolanofacundo@gmail.com>
2023-05-29 22:58:40 -0300

    upstream dev

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola</Text>
+      <Text>Dev</Text>
       <StatusBar style="auto" />
     </View>
   );
