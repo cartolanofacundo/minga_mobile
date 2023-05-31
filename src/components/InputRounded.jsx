05c140ad6bd6fb744041d8b8acd8476ff690887a
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-05-31 00:26:43 -0300

    register (#4)

--- src/components/InputRounded.jsx
+++ src/components/InputRounded.jsx
@@ -5,7 +5,7 @@ export function InputRounded({ data, handleData, error, name, icon }) {
     const [focus, setFocus] = useState(false)
     return (
         <>
-            <View className={`w-full rounded-full border-2 border-slate-300 px-6 py-2 relative bg-white flex flex-row justify-between items-center my-4 ${focus && "border-[#F97316]"} ${error && "border-red-500"}` }>
+            <View className={`w-full rounded-full border-2 border-slate-300 px-6 py-2 relative bg-white flex flex-row justify-between items-center my-2 ${focus && "border-[#F97316]"} ${error && "border-red-500"}` }>
                 <Text className={`mb-2 absolute top-[-12px] left-6 bg-white ${focus && "text-[#F97316]"} ${error && "text-red-500"}`}>{name.charAt(0).toUpperCase() + name.slice(1)}</Text>
                 <TextInput
                     className="w-3/4"
@@ -17,7 +17,7 @@ export function InputRounded({ data, handleData, error, name, icon }) {
                 />
                 <MaterialCommunityIcons name={icon} color={focus ? "#F97316" : error ? "#f56565" : "#000000"} size={25} />
             </View>
-           {error && <Text className="text-red-500">{error}</Text>} 
+           {error && <Text className="text-red-500 mb-1">{error}</Text>} 
         </>
 
 
