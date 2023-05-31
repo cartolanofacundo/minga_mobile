76c6a7b5f8c8402470bafb972e32f1fa03d27033
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-05-30 23:39:50 -0300

    login screen (#3)

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,15 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,Pressable } from 'react-native';
 
 export function HomeScreen({navigation}){
     return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View className="flex flex-col h-full w-full justify-center items-center bg-blue-500">
             <Text>HomeScreen</Text>
-            <Button 
-            title='Sign in'
+            <Pressable 
+            className="bg-violet-500 px-4 py-2 rounded-lg mt-4 text-white"
             onPress={() =>navigation.push("Login")}
-            />
+            > 
+            <Text className="text-white font-medium">Sing In</Text>
+            </Pressable>
         </View>
     )
 }
