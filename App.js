225b0f895ddd9c3b3680d822f8e709e82c9929ed
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-06-01 04:03:06 -0300

    Revert "Verify (#11)" (#12)
    This reverts commit f6c6b182cb8e565c7f0e45147652be3bd1b81fb0.

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
-import 'react-native-gesture-handler';
-
-
-import { RootStack } from './src/routes/RootStack';
-import { useEffect } from 'react';
-import { _retrieveData } from './src/utils/utils';
-import { Provider, useDispatch } from 'react-redux';
-import { store } from "./src/store/store"
-import authActions from './src/store/user/authActions';
-import categoryAction from "./src/store/categories/categoriesActions"
-const { sign_in_token } = authActions
-const {get_categories} = categoryAction
-export function AppWrapper() {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(get_categories())
-    _retrieveData({ key: "token" })
-      .then((res) => typeof res === "string" && dispatch(sign_in_token()))
-      .catch(err => console.log(err))
-  }, [])
-  return (
-
-    <RootStack />
-  )
-}
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-
   return (
-    <Provider store={store}>
-      <AppWrapper />
-    </Provider>
-
+    <View style={styles.container}>
+      <Text>hola</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
